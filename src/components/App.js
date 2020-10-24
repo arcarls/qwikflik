f6ed10665d57680429728b034e6fa3bc7522323b
Alex Carlson <alexcarlson@Alexs-MacBook-Air.local>
2020-10-24 16:47:22 -0500

    new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import { Router } from '@reach/router';
 import Header from './elements/Header';
 import Home from './Home';
+import Movie from './Movie';
+import NotFound from './NotFound';
 
 
 const GlobalStyle = createGlobalStyle`
@@ -13,11 +16,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 const App = () => (
-<>
+  <>
     <Header />
-    <Home />
+ 
+    <Router>
+      <Home path= "/" />
+      <Movie path= "/:movieId" />
+      <NotFound default />
+    </Router>
     <GlobalStyle />
-    </>
+  </>
 )
 
 export default App;
--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyledMovieThumb } from '../styles/StyledMovieThumb'
-
+import { Link } from '@reach/router';
 
 const MovieThumb = ({ image, movieId, clickable }) => ( 
   <StyledMovieThumb> 
       {clickable ? (
-        <img className="clickable" src={image} alt="moviethumb" />
-        ) : (
+        <Link to={`/${movieId}`}>
+          <img className="clickable" src={image} alt="moviethumb" />
+        </Link>
+      ) : (
           <img src={image} alot="moviethumb" />  
-        )}
+      )}
   </StyledMovieThumb>
 //if 'clickable is true, run this
 
