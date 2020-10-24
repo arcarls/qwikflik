8ef92ab36e19106bfec0dea77b348b068029a299
Alex Carlson <alexcarlson@Alexs-MacBook-Air.local>
2020-10-24 16:51:39 -0500

    build

---
+++ build/precache-manifest.ac5e7363d53407717b51174c37c8dab0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8834b3ab492f445f872c9cabf622affc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d13faf3b94b8efe5d8b",
+    "url": "/static/js/2.4280b0a3.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.4280b0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0485b0813537d692aa96",
+    "url": "/static/js/main.5c6d11ee.chunk.js"
+  },
+  {
+    "revision": "be4e832c6c80a169a970",
+    "url": "/static/js/runtime-main.b71d072a.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
